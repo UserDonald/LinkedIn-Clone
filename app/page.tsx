b32e1232b17c4334ee5f53abf480050edd21637c
djnash <djnash@iu.edu>
2024-04-24 19:38:43 -0400

    Implementing Main Page Grid Layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,19 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex">
-      <section>
+    <div className="grid grid-cols-8 mt-5 sm:px-5">
+      <section className="hidden md:inline md:col-span-2">
         <UserInformation />
       </section>
 
-      <section>
+      <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
         {/* Post Form */}
         {/* Post Feed */}
       </section>
 
-      <section>{/* Widget */}</section>
+      <section className="hidden xl:inline justify-center col-span-2">
+        {/* Widget */}
+      </section>
     </div>
   );
 }
