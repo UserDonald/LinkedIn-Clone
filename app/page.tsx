2e8a76022cc30b0d6eaaeb0790f2db9c2d397492
djnash <djnash@iu.edu>
2024-04-24 18:53:03 -0400

    Removing Main Page Default Classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <h1>LinkedIn Clone</h1>
     </main>
   );
